53ea96920bd51fba072fc9663ddaf8922d0e6cc2
Matthew Smailey <msmailey@networthservices.com>
2022-09-26 14:21:18 -0700

    $COMMIT_MESSAGE

---
+++ syncdown.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Shutdown command to be executed
+SHUTDOWN_COMMAND='shutdown -h now'
+
+# Location of folders file (one folder per line)
+FOLDER_FILE='folders.txt'
+
+COMMIT_MESSAGE='syncdown.sh auto commit'
+
+# Read folders
+cat $FOLDER_FILE | while read line; do
+	FOLDER=`echo "$line" | awk -F ':' '{print $1}'`
+	PUSH_COMMAND=`echo "$line" | awk -F ':' '{print $2}'`
+
+	`cd $FOLDER`
+	git add .
+	`git commit -m '$COMMIT_MESSAGE'`
+	`git push $PUSH_COMMAND`
+done
